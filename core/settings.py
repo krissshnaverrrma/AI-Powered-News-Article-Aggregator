28f744351d9680b2a43e2b1e8cac789c05ac58c0
Krishna Verma <krishna1290verma@gmail.com>
2026-01-16 18:23:29 +0530

    commit

--- core/settings.py
+++ core/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
         },
     },
 ]
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STORAGES = {
